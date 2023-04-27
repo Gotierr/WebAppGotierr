504f513e475316e83518a4b278e40b8c0d2ac9e2
Gotierr <gauthier.clet@viacesi.fr>
2023-04-27 23:35:51 +0200

    Adding front config

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Box } from '@mui/material';
+
+function App() {
+  return (
+ 
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Box>oe c greg</Box>
+      </header>
+    </div>
+  );
+}
+
+export default App;
