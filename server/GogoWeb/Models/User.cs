b81b63e578cebe94adcd7b8dd32d8e8fa14bf4d2
Gotierr <gauthier.clet@viacesi.fr>
2023-04-27 21:34:35 +0200

    Adding Back server

---
+++ server/GogoWeb/Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace GogoWeb.API.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    [Table("User")]
+    public class User
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public string Nationality { get; set; }
+    }
+}
